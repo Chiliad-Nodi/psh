e21bb42566676bc0df39fd7c4c2904c5db1034db
Chiliad-Nodi <chiliadnodi00@gmail.com>
2015-08-28 10:52:22 -0400

    Fixed bug in cd which prevented it from being chained

--- psh/commands/cd.py
+++ psh/commands/cd.py
@@ -1,4 +1,5 @@
 from psh.commands.core import BaseCommand, register_cmd
+import os;
 
 from psh.tree import TreeNode
 
@@ -7,8 +8,12 @@ from psh.tree import TreeNode
 class cd(BaseCommand):
     """Simple command that just changes directories to the specified path."""
 
+    def __init__(self, args=[]):
+        super(cd, self).__init__()
+        self.args = args
+    
     def call(self, *args, **kwargs):
         def output_generator():
-            os.chdir(os.path.join(os.path.abspath(os.path.curdir),args[1]))
+            os.chdir(os.path.join(os.path.abspath(os.path.curdir),self.args[0]))
             yield TreeNode(b'')
         return output_generator
